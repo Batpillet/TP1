016d119c69a07894ad5f7a2407eafc66d5f35194
Alex <alexander.hoffmann@edu.ece.fr>
2018-03-09 11:37:41 +0100

    blindage

--- src/Graphe.cpp
+++ src/Graphe.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 Graphe::Graphe(/*int ordre, std::vector<Arete*> matriceArete, std::vector<Sommet*> matriceSommet*/)
-:m_ordre(0), m_matriceArete(), m_matriceSommet()
+    :m_ordre(0), m_matriceArete(), m_matriceSommet()
 {
     //ctor
 }
@@ -20,9 +20,14 @@ void Graphe::RecupFichier(std::ifstream & influence, std::vector<Arete*> *m_matr
 
     while (influence >> a >> b)
     {
-        Sommet *A = new Sommet(a);
-        Sommet *B = new Sommet(b);
-        m_matriceArete->push_back(new Arete(A,B));
+        if((a[0] >= 'A' && a[0] <= 'Z') || (a[0] >= 'a' && a[0] <= 'z') && (b[0] >= 'A' && b[0] <= 'Z') || (b[0] >= 'a' && b[0] <= 'z'))
+        {
+            Sommet *A = new Sommet(a);
+            Sommet *B = new Sommet(b);
+            m_matriceArete->push_back(new Arete(A,B));
+        }
+        else
+            std::cout << "Erreur d'ecriture, reessayez." << std::endl;
     }
 }
 
